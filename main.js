2702d53783dd35cab0d6de9aedd0535a1c643731
pestonestor@gmail.com <pestonestor@gmail.com>
2023-06-17 12:10:27 -0300

    got them to walk with sprite animation while they race

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-  const newJump=(transitionEventObject)=>{
+function spriteWalk(horse) {
+  console.log('yes')
+  let sprite = document.getElementById(horse)
+  let cs = window.getComputedStyle(sprite)
+  let posX = cs.backgroundPositionX
+  let newPos = parseInt(posX) + 64
+  if (newPos > 192) {
+    newPos = 0
+  }
+  sprite.style.backgroundPositionX = `${newPos}px`
+}
+
+const newJump=(transitionEventObject)=>{
   let horseID=(transitionEventObject.target.getAttribute('value'))
   console.log(horseID)
   race(horseID)
@@ -13,13 +25,19 @@ const race=(id)=>{
   if (position<371){
     horse.addEventListener('transitionend',newJump)
     horse.style.left=`${position+(Math.floor(Math.random()*10+10))}px`
+    
+    
   }
   else{console.log('finish')}
 }
-
+for(let i=1;i<5;i++){
+setInterval(() => spriteWalk(`horse${i}`), 200)
+}
 //Don't exactly know what it's doing
 //Must understand if they are moving
 //independantly or should i use promises
 //Also want to pass value to newJump
 //without so much event.target hype
 //Also finetune movement & finish-line
+
+
